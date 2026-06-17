551703a9bdd6bdd0348dabb8e72b4e9e1b47e4e8
StolasRyan <paul.pbronx@gmail.com>
2026-06-17 19:52:48 +0300

    inviting and header section implemented

--- client/src/components/CustomChannelHeader.jsx
+++ client/src/components/CustomChannelHeader.jsx
@@ -1,9 +1,115 @@
-
+import { useChannelStateContext } from "stream-chat-react";
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { LockIcon, HashIcon, UserIcon, VideoIcon, PinIcon } from "lucide-react";
+import MembersModal from "./MembersModal";
+import PinnedMessagesModal from "./PinnedMessagesModal";
+import InviteModal from "./InviteModal";
 
 const CustomChannelHeader = () => {
-  return (
-    <div>CustomChannelHeader</div>
-  )
-}
+  const { channel } = useChannelStateContext();
+  const { user } = useUser();
+  const memberCount = Object.keys(channel.state.members).length;
 
-export default CustomChannelHeader
+  const [showInvite, setShowInvite] = useState(false);
+  const [showMembers, setShowMembers] = useState(false);
+  const [showPinnedMessages, setShowPinnedMessages] = useState(false);
+  const [pinnedMessages, setPinnedMessages] = useState([]);
+
+  const otherUsers = Object.values(channel.state.members).filter(
+    (member) => member.user.id !== user.id,
+  );
+  const isDM =
+    channel.data?.member_count === 2 && channel?.data?.id.includes("user_");
+
+  const handleShowPinned = async () => {
+    const channelState = await channel.query();
+    setPinnedMessages(channelState.pinned_messages);
+    setShowPinnedMessages(true);
+  };
+
+  const handleVideoCall = async () => {
+    if (channel) {
+      const callUrl = `${window.location.origin}/call/${channel.id}`;
+      await channel.sendMessage({
+        text: `I've joined a video call. Click here to join: ${callUrl}`,
+      });
+    }
+  };
+  return (
+    <div className="h-14 border-b border-gray-200 flex items-center justify-between px-4 bg-white">
+      <div className="flex items-center gap-3">
+        <div className="flex items-center gap-2">
+          {channel.data?.private ? (
+            <LockIcon className="size-4 text-[#616061]" />
+          ) : (
+            <HashIcon className="size-4 text-[#616061]" />
+          )}
+          {isDM && otherUsers?.user?.image && (
+            <img
+              src={otherUsers?.user?.image}
+              alt={otherUsers?.user?.name || otherUsers?.user?.id}
+              className="w-8 h-8 rounded-full object-cover mr-1"
+            />
+          )}
+          <span className="font-medium text-[#1D1C1D]">
+            {isDM ? otherUsers?.user?.name || otherUsers?.user?.id  : channel.data?.id}
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center gap-3">
+          <button
+          className="flex items-center gap-2 hover:bg-[#F8F8F8] py-1 px-2 rounded"
+          onClick={()=>setShowMembers(true)}
+          >
+            <UserIcon className="size-5 text-[#616061]" />
+            <span className="text-sm text-[#616061]">{memberCount}</span>
+          </button>
+
+          <button
+          className="hover:bg-[#F8F8F8] p-1 rounded"
+          onClick={handleVideoCall}
+          title="Start video-call"
+          >
+            <VideoIcon className="size-5 text-[#1264A3]" />
+          </button>
+
+          {channel?.data?.private && (
+            <button
+            className = 'btn btn-primary'
+            onClick={() => setShowInvite(true)}
+            > 
+              Invite
+            </button>
+          )}
+
+          <button
+          className="hover:bg-[#F8F8F8] p-1 rounded"
+          onClick={handleShowPinned}
+          >
+            <PinIcon className="size-4 text-[#616061]" />
+          </button>
+      </div>
+      {showMembers && (
+        <MembersModal
+          members={Object.values(channel.state.members)}
+          onClose={()=>setShowMembers(false)}
+        />
+      )}
+      {showPinnedMessages && (
+        <PinnedMessagesModal
+          pinnedMessages={pinnedMessages}
+          onClose={() => setShowPinnedMessages(false)}
+        />
+      )}
+      {showInvite && (
+        <InviteModal
+          channel={channel}
+          onClose={() => setShowInvite(false)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CustomChannelHeader;
---
+++ client/src/components/InviteModal.jsx
@@ -0,0 +1,132 @@
+import { useState, useEffect} from 'react';
+import { useChatContext } from 'stream-chat-react';
+import { XIcon } from 'lucide-react';
+import '../styles/stream-chat-theme.css';
+
+
+const InviteModal = ({channel, onClose}) => {
+    const {client} = useChatContext();
+    const [selectedMembers, setSelectedMembers] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const [error, setError] = useState('');
+    const [isInviting, setIsInviting] = useState(false);
+
+    useEffect(() => {
+       const fetchUsers = async () => {
+           setIsLoadingUsers(true);
+           setError('');
+           try {
+            const members = Object.keys(channel?.state.members)
+            const res = await client.queryUsers(
+                {id:{$nin:members}},
+                {name:1},
+                {limit:30}
+            );
+            setUsers(res.users);
+           } catch (error) {
+            console.error('Error fetching users:', error);
+            setError('Error fetching users');
+           }finally{
+            setIsLoadingUsers(false);
+           }
+       } 
+    },[channel, client]);
+
+    const handleInvite = async()=>{
+        if(selectedMembers.length === 0) return;
+        setIsInviting(true);
+        setError('');
+        try {
+            await channel.addMembers(selectedMembers);
+        } catch (error) {
+            setError('Error inviting users', error);
+            console.error('Error inviting users:', error);
+        }finally{
+            setIsInviting(false);
+        }
+    }
+  return (
+    <div className="create-channel-modal-overlay">
+        <div className="create-channel-modal">
+
+            <div className="create-channel-modal-header">
+                <h2>Invite Users</h2>
+                <button
+                onClick={onClose}
+                className="create-channel-modal__close"
+                >
+                    <XIcon  className="size-4" />
+                </button>
+            </div>
+
+            <div
+            className="create-channel-modal__form"
+            >
+            {isLoadingUsers && <p>Loading users...</p>}
+            {error && <p className="form-error">{error}</p>}
+            {users.length === 0 && !isLoadingUsers && <p>No users found</p> }
+            {users.length > 0 && (
+                users.map((user) => {
+                    const isChecked = selectedMembers.includes(user.id);
+                    return(
+                        <label
+                        key={user.id}
+                        className={`flex items-center gap-4 p-3 rounded-lg cursor-pointer transition-all shadow-sm bg-white hover:bg-[#f3f5ff] border-2
+                         ${isChecked ? 'border-[#611f69] bg-[#f3e6fa]' : 'border-gray-200'} 
+                        `}
+                        >
+                            <input
+                                type='checkbox'
+                                className='checkbox checkbox-primary checkbox-sm accent-[#611f69]'
+                                value={user.id}
+                                onChange={(e)=>{
+                                    if(e.target.checked){
+                                        setSelectedMembers([...selectedMembers, user.id])
+                                    }else{
+                                        setSelectedMembers(selectedMembers.filter((u)=> u!==user.id))
+                                    }
+                                }}
+                            />
+                            {user?.image ? (
+                                <img
+                                    src={user.image}
+                                    alt={user.name || user.id}
+                                    className="w-9 h-9 rounded-full object-cover border border-gray-200"
+                                />
+                            ):(
+                                <div className='size-9 rounded-full bg-gray-300 flex items-center justify-center text-white font-bold text-lg'>
+                                    {(user.name || user.id).charAt(0).toUpperCase()}
+                                </div>
+                            )}
+                            <span className='font-medium text-[#611f69] text-base'>
+                                {user.name || user.id}
+                            </span>
+                        </label>
+                    )
+                })
+            )}
+
+             <div className='create-channel-modal__actions mt-4'>
+                <button
+                className='btn btn-secondary'
+                onClick={onClose}
+                disabled={isInviting}
+                >
+                    Cancel
+                </button>
+                <button
+                className='btn btn-primary'
+                onClick={handleInvite}
+                disabled={isInviting || !selectedMembers.length}
+                >
+                    {isInviting ? 'Inviting...' : 'Invite'}
+                </button>
+             </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default InviteModal
